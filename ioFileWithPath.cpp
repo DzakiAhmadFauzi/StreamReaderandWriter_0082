f6cd33356e6d7bff6520c1adca05d43228b2cf62
DzakiAhmadFauzi <dzakfauzi229@gmail.com>
2025-06-10 15:45:07 +0700

    commit 14

--- ioFileWithPath.cpp
+++ ioFileWithPath.cpp
@@ -23,3 +23,13 @@ while (true) {
         if (baris == "q") break;
         // menulis dan memasukan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
+}
+    //selesai dalam menulis sekarang tutup filenya
+    outfile.close();
+    //Membuka file dalam mode membaca
+    ifstream infile;
+    //menunjuk ke sebuah file
+    infile.open(NamaFile, ios::in);
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    //jika file ada maka
+    if (infile.is_open())
